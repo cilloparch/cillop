6e691d090a426fe291823cc482ebd6cd9ca0353e
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-08-22 03:09:04 +0300

    :sparkles: add formats lib

---
+++ formats/formats.go
@@ -0,0 +1,12 @@
+package formats
+
+const (
+	DateYYYYMMDDHHMMSS = "2006-01-02 15:04:05"
+	DateYYYYMMDD       = "2006-01-02"
+	HHMMSS             = "15:04:05"
+	ISO8601            = "2006-01-02T15:04:05Z"
+	ISO8600            = "2006-01-02 15:04:05"
+	UNIQUE             = "20060102150405"
+	UNIQUE_SMALL       = "0102150405"
+	TIME_TO_SLUG       = "2006-01-02-15-04-05"
+)
