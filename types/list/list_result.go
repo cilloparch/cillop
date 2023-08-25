4ea7cd1b050f5bfde5abe71d1f7a07538174cbcf
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-08-25 23:42:44 +0300

    list: add  json mapping

--- types/list/list_result.go
+++ types/list/list_result.go
@@ -3,12 +3,12 @@ package list
 import "time"
 
 type Result[Type any] struct {
-	List          []Type
-	Total         int64
-	FilteredTotal int64
-	Page          int64
-	IsNext        bool
-	IsPrev        bool
+	List          []Type `json:"list"`
+	Total         int64  `json:"total"`
+	FilteredTotal int64  `json:"filteredTotal"`
+	Page          int64  `json:"page"`
+	IsNext        bool   `json:"isNext"`
+	IsPrev        bool   `json:"isPrev"`
 }
 
 type DateResult[Type any] struct {
