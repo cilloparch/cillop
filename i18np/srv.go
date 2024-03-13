5437bb1c79004f7a3f26c87ea9f7e4750bc4c46b
9ssi7 <info@ssibrahimbas.com>
2024-03-13 20:17:25 +0300

    feat: fix the i18np translation logic

--- i18np/srv.go
+++ i18np/srv.go
@@ -4,5 +4,9 @@ import "github.com/nicksnyder/go-i18n/v2/i18n"
 
 func (i *I18n) translate(c *i18n.LocalizeConfig, languages ...string) string {
 	localizer := i18n.NewLocalizer(i.b, languages...)
-	return localizer.MustLocalize(c)
+	res, err := localizer.Localize(c)
+	if err != nil {
+		return c.MessageID
+	}
+	return res
 }
